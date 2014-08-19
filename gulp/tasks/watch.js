a0ca3e0a8cae16f971ba974adfcae6203ac13110
jim-at-jibba <jim@justjibba.net>
2014-08-19 11:54:01 +0700

    Updated wach task in gulp

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-	gulp.watch('src/scss/**', ['compass']);
+	gulp.watch('src/scss/**', ['styles']);
 	gulp.watch('src/img/**', ['images']);
 	gulp.watch('src/js/**', ['scripts']);
 	gulp.watch('src/htdocs/**', ['copy']);
