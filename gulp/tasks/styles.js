1ab0206c77704d1cd23d7a0a19d5cccd9587e1bc
James G. Best <jim-at-jibba@users.noreply.github.com>
2015-01-27 14:55:22 +0700

    Update styles.js

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,21 +1,25 @@
 
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var prefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 var gulp         = require('gulp');
 var notify       = require('gulp-notify');
 var handleErrors = require('../util/handleErrors');
 
 // Where do you store your Sass files?
-var sassDir = 'src/scss';
+var sassDir = 'dev/scss';
 
 // Which directory should Sass compile to?
-var targetCSSDir = 'dist/css';
+var targetCSSDir = 'public/css';
 
 //Styles
-gulp.task('styles', function(){
-    return gulp.src(sassDir + '/styles.scss')
-        .pipe(sass({ style: 'extended','sourcemap=none': true, }).on('error', handleErrors))
-        .pipe(prefixer('last 10 version'))
-        .pipe(gulp.dest(targetCSSDir))
-        .pipe(notify({ message: 'All done, oh great one!'}));
+gulp.task('styles', function () {
+    gulp.src(sassDir + '/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            style: 'compressed',
+            errLogToConsole: true
+        })).on('error', handleErrors)
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(targetCSSDir));
 });
